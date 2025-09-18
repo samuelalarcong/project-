c4af9ae9992e5add633df0de4f1726434d227e5a
Samuel Alarcon Gambarte <114440076+samuelalarcong@users.noreply.github.com>
2025-09-18 17:59:57 +0200

    Update ingestion.py

--- fred/ingestion.py
+++ fred/ingestion.py
@@ -1,7 +1,10 @@
-from fredapi import Fred
 import pandas as pd
+from fredapi import Fred
 
 def fetch_series(series_id, api_key, start_date=None, end_date=None, frequency=None, reset_index=True):
+    """
+    Fetch a FRED series and return as a DataFrame.
+    """
     fred = Fred(api_key=api_key)
     data = fred.get_series(series_id, observation_start=start_date, observation_end=end_date)
     
@@ -9,6 +12,7 @@ def fetch_series(series_id, api_key, start_date=None, end_date=None, frequency=N
     df.index = pd.to_datetime(df.index)
     df.index.name = "date"
     
+    # Resample if frequency is specified
     if frequency:
         df = df.resample(frequency).last()
     
